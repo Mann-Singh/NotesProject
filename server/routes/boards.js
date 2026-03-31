640d14fe738dc570e9685c026487465a2750f7fd
Mann-Singh <manjot.singh.cs@outlook.com>
2026-03-31 16:49:26 -0700

    boards routing functions done

---
+++ server/routes/boards.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.router();
+
+router.get('/', (req, res) => {
+
+});
+
+router.post('/', (req, res) => {
+
+});
+
+router.get('/:id/', (req, res) => {
+
+});
+
+router.get('/:id/notes', (req, res) => {
+
+});
+
+router.post('/:id/notes', (req, res) => {
+
+});
+
